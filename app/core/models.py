7e8c468c1662108dbd60acae78cc78d7a26b87c6
Vali <valijon1998valijon@icloud.com>
2021-03-22 19:30:14 +0300

    models

--- app/core/models.py
+++ app/core/models.py
@@ -25,3 +25,32 @@ class Client(AbstractUser):
 
 class CargoFeature(models.Model):
     name = models.CharField(max_length=256)
+
+
+# Автомобиль
+class Car(models.Model):
+    car_number = models.PositiveSmallIntegerField() #номер автомобиля
+    type = models.CharField(max_length=30) # тип мерс или порше
+    lifting = models.PostitiveIntegerField() # грузоподьемность
+    car_type = models.CharField('gazel', 'fura') #тип газель или фура или легковушка
+
+# Рейтинг Автомобиля или же водителя???
+class Rating(models.Model):
+    star = models.ForeignKey(Car,on_delete=models.CASCADE, verbose_name='zvezda')
+    car_type = models.ForeignKey(Car,on_delete=models.CharField, verbose_name='gazel ili fura')
+
+    def __str__(self):
+        return f"{self.star} - {self.car_type}"
+
+
+# Отзывы
+class Reviews(models.Model):
+    email = models.EmailField()
+    name = models.CharField('imya', max_length=100)
+    text = models.TextField('soobshenie', max_length=5000)
+    parent =models.ForeignKey('self', verbose_name = 'roditel',on_delete=models.SET_NULL, blank=True, null=True)
+    car_type = models.ForeignKey(Car,verbose_name='gazel ili fura',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.car_type}"
+
