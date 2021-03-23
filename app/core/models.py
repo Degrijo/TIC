521dc34218cf862266cabcf34f7c166866aead3d
sevinchalimkhojaeva <alimkhodjaeva.s@mail.ru>
2021-03-23 12:16:45 +0300

    add model Shipping

--- app/core/models.py
+++ app/core/models.py
@@ -25,3 +25,16 @@ class Client(AbstractUser):
 
 class CargoFeature(models.Model):
     name = models.CharField(max_length=256)
+
+
+class Shipping(models.Model):
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    start_datetime = models.DateTimeField(auto_now_add=True, editable=False)
+    accept_datetime = models.DateTimeField(blank=True, null=True)
+    finish_datetime = models.DateTimeField(blank=True, null=True)
+    mass = models.PositiveSmallIntegerField()
+    cargo_features = models.ManyToManyField("core.CargoFeature",related_name="shippings")
+    sender = models.ForeignKey("core.Client", on_delete=models.CASCADE, related_name="packages")
+    recipient = models.ForeignKey("core.Client", on_delete=models.CASCADE, related_name="parcels")
+    employee = models.ForeignKey("core.Client", on_delete=models.CASCADE, related_name="orders", blank=True, null=True)
+    car = models.ForeignKey("core.Car", on_delete=models.SET_NULL, related_name="shippings", blank=True, null=True)
