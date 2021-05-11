d8b51d6e9b056b919aa6d9c64634081f439e1ec3
sevinchalimkhojaeva <alimkhodjaeva.s@mail.ru>
2021-05-11 22:50:18 +0300

    create email

--- config/urls.py
+++ config/urls.py
@@ -23,18 +23,18 @@ from app.core.views import SignUpClientView, LogInView, MainPageView, SignUpEmpl
     FinishOrderView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('main_page/', MainPageView.as_view(), name='main_page'),
-    path('signup_client/', SignUpClientView.as_view(), name='signup_client'),
-    path('signup_employee/', SignUpEmployeeView.as_view(), name='signup_employee'),
-    path('login/', LogInView.as_view(), name='login'),
-    path('create_order/', CreateOrderView.as_view(), name='create_order'),
-    path('list_my_orders/', ListMyOrdersView.as_view(), name='list_my_orders'),
-    path('logout/', LogOutView.as_view(), name='logout'),
-    path('order/<int:pk>/', DetailOrderView.as_view(), name='detail_order'),
-    path('list_actual_orders/', ListActualOrdersView.as_view(), name='list_actual_orders'),
-    path('accept_order/<int:pk>/', AcceptOrderView.as_view(), name='accept_order'),
-    path('create_car/', CreateCarView.as_view(), name='create_car'),
-    path('list_cars/', ListCarsView.as_view(), name='list_cars'),
-    path('finish_order/<int:pk>/', FinishOrderView.as_view(), name='finish_order')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('main_page/', MainPageView.as_view(), name='main_page'),
+                  path('signup_client/', SignUpClientView.as_view(), name='signup_client'),
+                  path('signup_employee/', SignUpEmployeeView.as_view(), name='signup_employee'),
+                  path('login/', LogInView.as_view(), name='login'),
+                  path('create_order/', CreateOrderView.as_view(), name='create_order'),
+                  path('list_my_orders/', ListMyOrdersView.as_view(), name='list_my_orders'),
+                  path('logout/', LogOutView.as_view(), name='logout'),
+                  path('order/<int:pk>/', DetailOrderView.as_view(), name='detail_order'),
+                  path('list_actual_orders/', ListActualOrdersView.as_view(), name='list_actual_orders'),
+                  path('accept_order/<int:pk>/', AcceptOrderView.as_view(), name='accept_order'),
+                  path('create_car/', CreateCarView.as_view(), name='create_car'),
+                  path('list_cars/', ListCarsView.as_view(), name='list_cars'),
+                  path('finish_order/<int:pk>/', FinishOrderView.as_view(), name='finish_order'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
